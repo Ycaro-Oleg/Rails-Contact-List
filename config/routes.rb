a4c469580c630f25fd4e5a3b30cb90a84833ebc7
ycaro-oleg <ycaro.oleg@gmail.com>
2023-11-09 12:03:19 -0300

    Adjusting routes.rb file and added new controller sessions with new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :users, only: [:new, :create]
-  
-  root 'static_pages#index'
 
-  get 'sobre', to: 'static_pages#sobre'
-  get 'contato', to: 'static_pages#contato'
+  get 'sessions/new'
+  root            to: 'static_pages#index'
+  get 'sobre',    to: 'static_pages#sobre'
+  get 'contato',  to: 'static_pages#contato'
+
+  get 'cadastro', to: 'users#new'
+  get 'entrar',   to: 'sessions#new'
+  post 'entrar',  to: 'sessions#create'
+  delete 'sair',  to: 'sessions#destroy'
+
+  resources :users, only: [:show, :new, :create, :edit, :update] do
+    resources :contacts, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+
 end
